6713c2a1fb962a6486b001967e82527af33a8b2d
Axel Schneewind <axel.schneewind@protonmail.com>
2026-01-13 23:19:17 +0100

    remove warning from custom dictionary

--- Assets/Scripts/PlayerStats/StatScalingDefinition.cs
+++ Assets/Scripts/PlayerStats/StatScalingDefinition.cs
@@ -85,7 +85,6 @@ public class KeyValueStore<Key, Value>
         // check for missing definitions
         for (int i = 0; i < _defs.Length; i++) {
             if (_defs[i] == null) {
-                Debug.LogWarning("scaling undefined for " + Enum.GetValues(typeof(Key)).GetValue(i));
                 _defs[i] = default;
             }
         }
