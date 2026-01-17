58a822e032a3bfe99e32852c8ff00081933cd5ec
Axel Schneewind <axel.schneewind@protonmail.com>
2026-01-17 16:20:21 +0100

    allow configuration of different ui states

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 using UnityEngine.InputSystem;
 
 // class to handle ui state:
@@ -7,7 +8,7 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
 
-    // ui elements
+    // ui elements (convenient accessors for other scripts)
     public GameObject playerStats { get; private set; }
     public GameObject hotbar { get; private set; }
     public GameObject inventoryUI { get; private set; }
@@ -17,74 +18,132 @@ public class UIManager : MonoBehaviour
     public bool inventoryOpen { get => inventoryUI != null && inventoryUI.activeInHierarchy; }
     public bool upgradesOpen { get => upgradesUI != null && upgradesUI.activeInHierarchy; }
 
+    // for configuring ui element visibility
+    [System.Serializable]
+    public struct UiElementConfig {
+        public GameObject uiElement;
+        public UIState visibleIn;
+    }
+
+    [Header("UI elements")]
+    public UiElementConfig[] uiElements;
+    [Header("Cursor")]
+    public UIState cursorUnlocked;
+
+    // for configuring action map toggling
+    [System.Serializable]
+    public struct ActionMapConfig {
+        public string name;
+        public UIState enabledIn;
+
+        [NonSerialized]
+        public InputActionMap actionMap;
+    }
+
+    [System.Serializable]
+    public struct ActionConfig {
+        public string name;
+        public UIState enabledIn;
+
+        [NonSerialized]
+        public InputAction action;
+    }
+
+    [Header("Input Actions/Action Maps")]
+    public ActionMapConfig[] actionMaps;
+    [Tooltip("Will be applied after toggling action maps")]
+    public ActionConfig[] actions;
+
     // set of possible ui states
-    public enum UIState {
-        Gameplay,
-        Inventory,
-        Upgrades,
-        Death,
-        Pause
-    };
+    [Flags]
+    public enum UIState : int {
+        None = 0,
+        Gameplay = 1,
+        Inventory = 4,
+        Upgrades = 8,
+        Pause = 64,
+        Death = 128
+    }
     public UIState currentState { get; private set; }
 
 
     public void SwitchToGameplay() {
-        upgradesUI.SetActive(false);
-        inventoryUI.SetActive(false);
-        deathScreen.SetActive(false);
-
-        playerStats.SetActive(true);
-        hotbar.SetActive(true);
-
         currentState = UIState.Gameplay;
+        ApplyState();
     }
 
     public void SwitchToInventory() {
-        playerStats.SetActive(false);
-        hotbar.SetActive(false);
-        upgradesUI.SetActive(false);
-
-        inventoryUI.SetActive(true);
-
         currentState = UIState.Inventory;
+        ApplyState();
     }
 
     public void SwitchToUpgrades() {
-        playerStats.SetActive(false);
-        hotbar.SetActive(false);
-        inventoryUI.SetActive(false);
-
-        upgradesUI.SetActive(true);
-
-        currentState = UIState.Inventory;
+        currentState = UIState.Upgrades;
+        ApplyState();
     }
 
-
     public void SwitchToDeathScreen() {
-        playerStats.SetActive(false);
-        hotbar.SetActive(false);
-
-        inventoryUI.SetActive(false);
-        upgradesUI.SetActive(false);
-
-        deathScreen.SetActive(true);
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-
         currentState = UIState.Death;
+        ApplyState();
+    }
+
+    public void SwitchToPauseScreen() {
+        currentState = UIState.Pause;
+        ApplyState();
     }
 
     void Awake() {
         Instance = this;
+
         playerStats = GameObject.Find("Player Stats");
         hotbar = GameObject.Find("Hotbar");
-
         inventoryUI = GameObject.Find("Inventory");
         upgradesUI = GameObject.Find("Upgrades");
-
         deathScreen = GameObject.Find("Death Screen");
+
+        // get actions/maps from names
+        for (int i = 0; i < actionMaps.Length; i++) {
+            actionMaps[i].actionMap = InputSystem.actions.FindActionMap(actionMaps[i].name, true);
+        }
+        for (int i = 0; i < actions.Length; i++) {
+            actions[i].action = InputSystem.actions.FindAction(actions[i].name, true);
+        }
 
         SwitchToGameplay();
     }
 
+
+    private void ApplyState() {
+        UpdateVisibility();
+        UpdateInputSystem();
+    }
+
+    private void UpdateVisibility () {
+        for (int i = 0; i < uiElements.Length; i++) {
+            uiElements[i].uiElement.SetActive((uiElements[i].visibleIn & currentState) != UIState.None);
+        }
+
+        Cursor.lockState = ((cursorUnlocked & currentState) != UIState.None) ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = Cursor.lockState != CursorLockMode.Locked;
+    }
+
+    private void UpdateInputSystem() {
+        for (int i = 0; i < actionMaps.Length; i++) {
+            if ((actionMaps[i].enabledIn & currentState) != UIState.None) {
+                actionMaps[i].actionMap.Enable();
+            } else {
+                actionMaps[i].actionMap.Disable();
+            }
+        }
+
+        for (int i = 0; i < actions.Length; i++) {
+            if ((actions[i].enabledIn & currentState) != UIState.None) {
+                actions[i].action.Enable();
+            } else {
+                actions[i].action.Disable();
+            }
+        }
+    }
+
 }
+
