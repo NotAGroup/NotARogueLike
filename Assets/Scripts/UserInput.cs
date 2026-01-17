596f813ca5b2926830141b6284548da4e43e4fb8
Axel Schneewind <axel.schneewind@protonmail.com>
2026-01-17 16:20:02 +0100

    switch to action maps

--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System;
+using System.Collections.Generic;
 
 public class UserInput : MonoBehaviour
 {
@@ -16,271 +18,227 @@ public class UserInput : MonoBehaviour
         get => !uiController.upgradesOpen && !uiController.inventoryOpen && !player.isDead;
     }
 
-    private bool aim;
-    private bool interact;
-    private bool jump;
-    private bool run;
-    private bool slide;
-    private bool sneak;
-    private bool escape;
+    // movement actions
+    private InputAction moveAction;
+    private InputAction rotateAction;
 
-    private int itemID;
+    // movement modifiers
+    private InputAction sneakAction;
+    private InputAction sprintAction;
+    private InputAction slideAction;
+    private InputAction jumpAction;
 
-    // whether a ui should be toggled in this frame
-    private bool toggleUI;
-    private bool switchUI;
-    private bool inventory;
-    private bool upgrades;
+    private InputAction interactAction;
 
-    // values for ui
-    private Vector2 uiDirection;
-    private bool uiSelect;
+    // combat
+    private InputAction changeAttackAction;
+    private InputAction attackAction;
+    private InputAction strikeAction;
+    private InputAction aimAction;
+    private InputAction shootAction;
 
-    public float mouseSensitivity = 0.1f;
-    public float stickSensitivity = 150f;
+    // *-state -> gameplay, gameplay -> inventory
+    private InputAction toggleUIAction;
+    // inventory <-> upgrades
+    private InputAction switchUIAction;
+
+    // item usage action
+    private InputAction item1Action;
+    private InputAction item2Action;
+    private InputAction item3Action;
+    private InputAction item4Action;
+
+    // any state -> selected-ui -> gameplay
+    private InputAction toggleUpgradesAction;
+    private InputAction toggleInventoryAction;
+    private InputAction togglePauseAction;
+
+    // ui actions
+    private InputAction uiSelectAction;
+    private InputAction uiMoveAction;
+    private InputAction uiCloseAction;
+
+    public float lookSensitivity = 1f;
+
+    //
+    private float uiNavigateTimer;
+    [Tooltip("How fast the ui should be navigated (in Hz)")]
+    public float uiNavigateRepeatRate;
 
     void Awake()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
         playerUpgrades = GameObject.Find("Player").GetComponent<PlayerUpgrades>();
+
         uiController = GetComponent<UIManager>();
         inventoryUI = uiController.inventoryUI.GetComponent<InventoryUI>();
         upgradeUI = uiController.upgradesUI.GetComponent<UpgradeUI>();
 
-        rotation = Vector2.zero;
+        FindActions();
+    }
 
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+    void FindActions() 
+    {
+        // find input actions
+		moveAction = InputSystem.actions.FindAction("Move", true);
+		rotateAction = InputSystem.actions.FindAction("Look", true);
+
+		jumpAction = InputSystem.actions.FindAction("Jump", true);
+		sneakAction = InputSystem.actions.FindAction("Crouch", true);
+		sprintAction = InputSystem.actions.FindAction("Sprint", true);
+		slideAction = InputSystem.actions.FindAction("Slide", true);
+		interactAction = InputSystem.actions.FindAction("Interact", true);
+		strikeAction = InputSystem.actions.FindAction("Strike", true);
+		attackAction = InputSystem.actions.FindAction("Attack", true);
+		changeAttackAction = InputSystem.actions.FindAction("ChangeAttack", true);
+		aimAction = InputSystem.actions.FindAction("Aim", true);
+		shootAction = InputSystem.actions.FindAction("Shoot", true);
+
+		toggleUIAction = InputSystem.actions.FindAction("ToggleUI", true);
+		switchUIAction = InputSystem.actions.FindAction("SwitchUI", true);
+
+		item1Action = InputSystem.actions.FindAction("Item1", true);
+		item2Action = InputSystem.actions.FindAction("Item2", true);
+		item3Action = InputSystem.actions.FindAction("Item3", true);
+		item4Action = InputSystem.actions.FindAction("Item4", true);
+
+		toggleUpgradesAction = InputSystem.actions.FindAction("ToggleUpgrades", true);
+		toggleInventoryAction = InputSystem.actions.FindAction("ToggleInventory", true);
+		togglePauseAction = InputSystem.actions.FindAction("TogglePause", true);
+  
+		uiMoveAction = InputSystem.actions.FindAction("Navigate", true);
+		uiSelectAction = InputSystem.actions.FindAction("Submit", true);
+		uiCloseAction = InputSystem.actions.FindAction("Cancel", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Reset variables
-        direction = Vector2.zero;
-        uiDirection = Vector2.zero;
-
-        aim = false;
-        interact = false;
-        jump = false;
-        run = false;
-        slide = false;
-        sneak = false;
-        inventory = false;
-        upgrades = false;
-        toggleUI = false;
-        switchUI = false;
-        uiSelect = false;
-
-        itemID = -1;
-
-        KeyboardInput();
-        GamepadInput();
-        MouseInput();
-
         // Apply inputs
-        if (controlPlayer) {
-            player.Run(run);
-            player.Slide(slide);
-            player.Sneak(sneak);
+        if (controlPlayer) 
+        {
+            player.Run(sprintAction.IsPressed());
 
-            player.Aim(aim);
+            player.Slide(slideAction.IsPressed());
+            player.Sneak(sneakAction.IsPressed());
+
+            player.Aim(aimAction.IsPressed());
+
+            Vector2 direction = moveAction.ReadValue<Vector2>();
+            Vector2 rotation  = lookSensitivity * rotateAction.ReadValue<Vector2>();
+
             player.Rotate(rotation);
             player.Move(direction);
+        }
 
-            if (itemID > -1)
+        if (!controlPlayer) 
+        {
+            uiNavigateTimer -= Time.deltaTime;
+
+            Vector2 uiDirection = uiMoveAction.ReadValue<Vector2>();
+            if (uiDirection != Vector2.zero && uiNavigateTimer <= 0f) 
             {
-                player.UseItem(itemID);
+                // control upgrades ui
+                if (uiController.upgradesOpen) 
+                {
+                    upgradeUI.MoveSelection(uiDirection);
+                    uiNavigateTimer = 1f / uiNavigateRepeatRate;
+                }
+    
+                // control inventory ui
+                if (uiController.inventoryOpen) 
+                {
+                    inventoryUI.MoveSelection(uiDirection);
+                    uiNavigateTimer = 1f / uiNavigateRepeatRate;
+                }
             }
 
-            if (interact)
-            {
-                player.Interact();
-            }
+            if (uiDirection == Vector2.zero)
+                uiNavigateTimer = 0f;
+        } 
 
-            if (jump)
+        // items
+        if (item1Action.WasPerformedThisFrame()) { this.player.UseItem(0); }
+        if (item2Action.WasPerformedThisFrame()) { this.player.UseItem(1); }
+        if (item3Action.WasPerformedThisFrame()) { this.player.UseItem(2); }
+        if (item4Action.WasPerformedThisFrame()) { this.player.UseItem(3); }
+
+        if (jumpAction.WasPerformedThisFrame()) { this.player.Jump(); }
+
+        if (interactAction.WasPerformedThisFrame()) {this.player.Interact(); }
+
+        // input using selected attack
+        if (changeAttackAction.WasPerformedThisFrame()) {this.player.ChangeAttack(); }
+        if (attackAction.WasPerformedThisFrame()) {this.player.Attack(); }
+
+        // attack-type-specific inputs 
+        if (strikeAction.WasPerformedThisFrame()) {
+            this.player.ChangeAttack(Player.AttackType.Hit);
+            this.player.Attack();
+        }
+        if (shootAction.WasPerformedThisFrame()) {
+            this.player.ChangeAttack(Player.AttackType.Shoot);
+            this.player.Attack();
+        }
+
+
+        if (uiSelectAction.WasPerformedThisFrame()) {
+            if (this.uiController.upgradesOpen) 
             {
-                player.Jump();
+                // control upgrades ui
+                this.playerUpgrades.TryUpgrade(this.upgradeUI.selectedStat);
+            }
+            else if (this.uiController.inventoryOpen) 
+            {
+                // control inventory ui
+                this.inventoryUI.ToggleItemGrabbed();
             }
         }
 
-        // control upgrades ui
-        if (uiController.upgradesOpen) {
-            upgradeUI.MoveSelection(uiDirection);
-
-            if (uiSelect)
-                playerUpgrades.TryUpgrade(upgradeUI.selectedStat);
-        }
-
-        // control inventory ui
-        if (uiController.inventoryOpen) {
-            if (uiDirection != Vector2.zero)
-                inventoryUI.MoveSelection(uiDirection);
-            
-            if (uiSelect)
-                inventoryUI.ToggleItemGrabbed();
-        }
-
-        // handle switching between UIs
-        if (switchUI) {
+        if (switchUIAction.WasPerformedThisFrame()) {
+            // handle switching between UIs
             if (uiController.upgradesOpen) {
-                uiController.SwitchToInventory();
+                this.uiController.SwitchToInventory();
             } else if (uiController.inventoryOpen) {
-                uiController.SwitchToUpgrades();
+                this.uiController.SwitchToUpgrades();
             }
         }
-
-        if (escape) {
-            uiController.SwitchToGameplay();
+        
+        if (uiCloseAction.WasPerformedThisFrame()) {
+            // handle closing of ui
+            this.uiController.SwitchToGameplay();
         }
 
-        // handle toggling of ui
-        if (toggleUI) {
-            if (uiController.upgradesOpen || uiController.inventoryOpen) {
-                uiController.SwitchToGameplay();
+        if (toggleUIAction.WasPerformedThisFrame()) {
+            // handle toggling of ui
+            if (this.uiController.upgradesOpen || this.uiController.inventoryOpen) {
+                this.uiController.SwitchToGameplay();
             } else {
-                uiController.SwitchToInventory();
+                this.uiController.SwitchToInventory();
             }
         }
 
-        // toggle upgrade ui
-        if (upgrades) {
-            if (uiController.upgradesOpen) {
-                uiController.SwitchToGameplay();
+        if (toggleUpgradesAction.WasPerformedThisFrame()) {
+            // toggle upgrade ui
+            if (this.uiController.upgradesOpen) {
+                this.uiController.SwitchToGameplay();
             } else {
-                uiController.SwitchToUpgrades();
+                this.uiController.SwitchToUpgrades();
             }
         }
 
-        // toggle inventory ui
-        if (inventory) { 
-            if (uiController.inventoryOpen) {
-                uiController.SwitchToGameplay();
+        if (toggleInventoryAction.WasPerformedThisFrame()) {
+            // toggle inventory ui
+            if (this.uiController.inventoryOpen) {
+                this.uiController.SwitchToGameplay();
             } else {
-                uiController.SwitchToInventory();
+                this.uiController.SwitchToInventory();
             }
         }
-    }
 
-    void GamepadInput()
-    {
-        Gamepad gamepad = Gamepad.current;
 
-        if (gamepad == null)
-        {
-            return;
-        }
-
-        bool startButton = gamepad.startButton.isPressed;
-
-        // Movement
-        direction += gamepad.leftStick.ReadValue();
-
-        // Camera
-        rotation += gamepad.rightStick.ReadValue() * Time.deltaTime * stickSensitivity;
-        aim |= gamepad.leftTrigger.IsPressed();
-
-        // Attack
-        if (gamepad.rightTrigger.IsPressed())
-        {
-            player.Attack(Player.AttackType.Shoot);
-        }
-
-        if (gamepad.xButton.isPressed)
-        {
-            player.Attack(Player.AttackType.Hit);
-        }
-
-        interact |= gamepad.bButton.wasPressedThisFrame;
-        jump |= gamepad.aButton.wasPressedThisFrame;
-        run |= gamepad.leftStickButton.isPressed;
-        slide |= gamepad.rightShoulder.isPressed;
-        sneak |= gamepad.leftShoulder.isPressed;
-
-        // Item selection and ui navigation
-        if (gamepad.dpad.left.wasPressedThisFrame)
-        {
-            uiDirection.x -= 1;
-            itemID = 0;
-        }
-        if (gamepad.dpad.up.wasPressedThisFrame)
-        {
-            uiDirection.y += 1;
-            itemID = 1;
-        }
-        if (gamepad.dpad.right.wasPressedThisFrame)
-        {
-            uiDirection.x += 1;
-            itemID = 2;
-        }
-        if (gamepad.dpad.down.wasPressedThisFrame)
-        {
-            uiDirection.y -= 1;
-            itemID = 3;
-        }
-
-        // from gameplay, open inventory using y-button
-        toggleUI |= gamepad.yButton.wasPressedThisFrame;
-
-        // from inventory/upgrades, switch using shoulder buttons
-        switchUI |= gamepad.rightShoulder.wasPressedThisFrame;
-        switchUI |= gamepad.leftShoulder.wasPressedThisFrame;
-
-        uiSelect |= gamepad.aButton.wasPressedThisFrame;
-    }
-
-    void KeyboardInput()
-    {
+        // 
         Keyboard keyboard = Keyboard.current;
-
-        if (keyboard == null)
-        {
-            return;
-        }
-
-        escape = keyboard.escapeKey.isPressed;
-
-        // Movement
-        if (keyboard.sKey.isPressed || keyboard.downArrowKey.isPressed)
-        {
-            direction.y -= 1;
-        }
-        if (keyboard.aKey.isPressed || keyboard.leftArrowKey.isPressed)
-        {
-            direction.x -= 1;
-        }
-        if (keyboard.dKey.isPressed || keyboard.rightArrowKey.isPressed)
-        {
-            direction.x += 1;
-        }
-        if (keyboard.wKey.isPressed || keyboard.upArrowKey.isPressed)
-        {
-            direction.y += 1;
-        }
-
-        interact |= keyboard.fKey.wasPressedThisFrame;
-        jump |= keyboard.spaceKey.wasPressedThisFrame;
-        run |= keyboard.leftShiftKey.isPressed;
-        slide |= keyboard.leftAltKey.isPressed;
-        sneak |= keyboard.leftCtrlKey.isPressed;
-
-        // Item selection
-        if (keyboard.digit1Key.wasPressedThisFrame)
-        {
-            itemID = 0;
-        }
-        if (keyboard.digit2Key.wasPressedThisFrame)
-        {
-            itemID = 1;
-        }
-        if (keyboard.digit3Key.wasPressedThisFrame)
-        {
-            itemID = 2;
-        }
-        if (keyboard.digit4Key.wasPressedThisFrame)
-        {
-            itemID = 3;
-        }
 
 #if UNITY_EDITOR
         // For testing save/load
@@ -293,56 +251,6 @@ public class UserInput : MonoBehaviour
             GameSaver.load();
         }
 #endif
-
-        // ui
-        if (keyboard.sKey.wasPressedThisFrame || keyboard.downArrowKey.wasPressedThisFrame)
-        {
-            uiDirection.y -= 1;
-        }
-        if (keyboard.aKey.wasPressedThisFrame || keyboard.leftArrowKey.wasPressedThisFrame)
-        {
-            uiDirection.x -= 1;
-        }
-        if (keyboard.dKey.wasPressedThisFrame || keyboard.rightArrowKey.wasPressedThisFrame)
-        {
-            uiDirection.x += 1;
-        }
-        if (keyboard.wKey.wasPressedThisFrame || keyboard.upArrowKey.wasPressedThisFrame)
-        {
-            uiDirection.y += 1;
-        }
-
-        // from inventory/upgrades, switch using tab
-        switchUI  |= keyboard.tabKey.wasPressedThisFrame;
-
-        upgrades  |= keyboard.uKey.wasPressedThisFrame;
-        inventory |= keyboard.eKey.wasPressedThisFrame;
-
-        uiSelect  |= keyboard.spaceKey.wasPressedThisFrame;
     }
 
-    void MouseInput()
-    {
-        Mouse mouse = Mouse.current;
-
-        if (mouse == null)
-        {
-            return;
-        }
-
-        // Camera
-        rotation += mouse.delta.ReadValue() * mouseSensitivity;
-        aim |= mouse.rightButton.isPressed;
-
-        // Attack
-        if (mouse.leftButton.isPressed)
-        {
-            player.Attack();
-        }
-
-        if (mouse.scroll.down.IsPressed() || mouse.scroll.up.IsPressed())
-        {
-            player.ChangeAttack();
-        }
-    }
 }
