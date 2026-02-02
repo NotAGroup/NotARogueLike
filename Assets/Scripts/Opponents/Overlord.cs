0ee3008e8dd6b0f31646713dedc0140995271b43
st149535 <st149535@stud.uni-stuttgart.de>
2026-02-02 11:05:32 +0100

    Fixes multiple spawning of minions

--- Assets/Scripts/Opponents/Overlord.cs
+++ Assets/Scripts/Opponents/Overlord.cs
@@ -137,8 +137,10 @@ public class Overlord : Opponent
                     StopCoroutine(initializePhaseCoroutine);
                     initializedPhase = true;
                 }
-
-                initializePhaseCoroutine = StartCoroutine(InitializePhase());
+                else
+                {
+                    initializePhaseCoroutine = StartCoroutine(InitializePhase());
+                }
             }
         }
 
