ca06965f38273d272a01fbb1dbbe26eff8fd5c7a
st149799 <st149799@stud.uni-stuttgart.de>
2026-02-04 00:07:43 +0100

    Boss door animation (#16)
    * small changes for debugging
    
    REMOVE for production
    
    * some automatic changes from unity.
    
    no idea why
    
    * add aniamtion
    
    * new animation system atemp
    
    * fix animation
    
    * wait for door open
    
    * Revert "small changes for debugging"
    
    This reverts commit 0d812a2a25ec85403a7087dffd9df5b5afeb5f49.
    
    * remove debug key
    
    ---------
    
    Co-authored-by: David Ruff <develop@ruffdd.de>

--- Assets/Scripts/BossDoor.cs
+++ Assets/Scripts/BossDoor.cs
@@ -8,19 +8,40 @@ public class BossDoor : MonoBehaviour
     public string closedInteractionText;
     public string openInteractionText;
 
+    public Animator openAnimation;
+
+    private bool open = false;
+
     public void Enable() {
         isEnabled = true;
         GetComponent<InteractionHint>().Text = openInteractionText;
     }
 
-    public void Interact()
+    public void Awake()
     {
+    }
+
+    public void Interact()
+    { 
         if (!isEnabled) return;
 
-        GetComponent<Collider>().enabled = false;
+        Debug.Log("Floor cleared, opening Boss Room");
+        openAnimation.Play("Open",0,0.0f);
+        open=true;
     }
 
     void Start() {
         GetComponent<InteractionHint>().Text = closedInteractionText;
+    }
+
+    void Update() 
+    {
+        AnimatorStateInfo stateInfo = openAnimation.GetCurrentAnimatorStateInfo(0);
+        if(open && stateInfo.normalizedTime > 1f && stateInfo.IsName("Open"))
+        {
+            GetComponent<Collider>().enabled = false;
+            Debug.Log("Door finish opening");
+        }
+            
     }
 }
